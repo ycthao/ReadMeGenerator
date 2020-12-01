ebd480a29638728876585d0aa8be7f5f7a9a39e4
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-01 00:02:26 -0600

    Fixed example to write README.md

--- index.js
+++ index.js
@@ -2,19 +2,26 @@
 const fs = require('fs');
 // call for inquirer
 const inquirer = require('inquirer');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is your user name?',
-            name: 'username',
-        },
-    ])
-    .then((data) =>
-        fs.writeFile('./Readme/README.md', data, (err) =>
-            err ? console.error(err) : console.log('Success!')
-        )
-    );
+const promptUser = () =>
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    }
+]);
+
+const generateREADME = (answers) =>
+    `My name is ${answers.name}`
+    ;
+
+promptUser()
+    .then((answers) => writeFileAsync('./Readme/README.md', generateREADME(answers)))
+    .then(() => console.log('Successfully wrote to index.html'))
+    .catch((err) => console.error(err));
