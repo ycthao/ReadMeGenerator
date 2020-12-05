b433aff1828b7bd95607f2f8c90a33977874a048
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-04 18:23:28 -0600

    Adding all headings, and license

--- index.js
+++ index.js
@@ -12,16 +12,84 @@ const promptUser = () =>
   inquirer.prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'What is your name?',
-    }
-]);
+      name: 'title',
+      message: 'What is the name of your project?',
+    },
+    {
+      type: 'input',
+      name: 'author',
+      message: 'What is the name of the author?',
+    },
+    {
+      type: 'input',
+      name: 'year',
+      message: 'What year is this project written?',
+    },
+  ]);
+
 
 const generateREADME = (answers) =>
-    `My name is ${answers.name}`
-    ;
+  `# ${answers.title} # 
+
+## Description ## 
+
+
+## Table of Contents ##
+
+
+## Installation ##
+
+
+## Usage ##
+
+
+## License ##
+
+
+## Contributing ##
+
+
+## Tests ##
+
+
+## Credits ##
+
+
+## Author ##
+
+
+## License ##
+
+MIT License
+
+Copyright (c) [${answers.year}] [${answers.author}]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Badges ##
+
+
+
+    `
+  ;
 
 promptUser()
-    .then((answers) => writeFileAsync('./Readme/README.md', generateREADME(answers)))
-    .then(() => console.log('Successfully wrote to index.html'))
-    .catch((err) => console.error(err));
+  .then((answers) => writeFileAsync('./Readme/README.md', generateREADME(answers)))
+  .then(() => console.log('Successfully wrote to index.html'))
+  .catch((err) => console.error(err));
