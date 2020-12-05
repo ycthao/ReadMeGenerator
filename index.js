293688236fa4956174749842f9837db5bf9f48c3
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2020-12-04 23:39:21 -0600

    adding more license to list

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const promptUser = () =>
       type: 'list',
       message: 'Choose your license?',
       name: 'license',
-      choices: ['MIT', 'Apache'],
+      choices: ['MIT', 'Apache', 'ISC', 'GNU'],
     },
     { // asking for email
       type: 'input',
@@ -108,7 +108,7 @@ ${answers.test}
 
 ## License 
 
-${getLicense}
+${answers.license}
 
 ## Author 
 
@@ -120,60 +120,6 @@ GitHub: ${answers.github}
 
 For additional questions, please email me at ${answers.email} with "${answers.title} question" in the subject line.
   `; // closing generateREADME
-
-
-
-const getLicense = (answers) => {
-  if (answers.license.choices === "MIT") {
-    const MIT = 
-    `
-MIT License
-
-Copyright (c) [${answers.year}] [${answers.author}]
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-`
-    return MIT;
-  } else if (answers.license === "Apache") {
-    const apache =
-    `
-Copyright [yyyy] [name of copyright owner]
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-    `
-  }
-
-
-}
-
-
-
 
 
 promptUser()
